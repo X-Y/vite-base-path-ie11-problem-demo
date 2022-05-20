15f2abb8c472724a6eeef66e9e18b095194076a5
Xun Yang <xun.yang@modernaforsakringar.se>
2022-05-20 13:49:54 +0200

    Make lazy loading more obvious.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import { useState } from 'react'
 import DateSelect from "./components/DateSelect/DateSelect";
 
 function App() {
+  const [reveal, setReveal] = useState(false);
+  const onClick = () => {
+    setReveal(true);
+  }
   return (
     <div>
-      <DateSelect />
+      <button onClick={onClick}>Reveal lazy comp</button>
+      {reveal && <DateSelect />}
     </div>
   )
 }
